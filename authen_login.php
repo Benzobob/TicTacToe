281c1629d21891d1404f1c9f9c6fc78b9c5feebe
benzobob <Auloocai1>
2019-11-13 13:54:32 +0000

    Created authen_login.php Login now works with DB

--- authen_login.php
+++ authen_login.php
@@ -3,7 +3,11 @@ include_once 'WebServiceClient.php';
 $username = $_POST["user_id"];
 $password = $_POST["user_pass"];
 
+//Call the web service 'login' method
 $result = $client->login(array("username" => $username, "password" => $password));
+
+/* Check if login details are valid. If not, output an error and refresh login page.
+ * If details are correct, then print success message and load dashboard page. */
 if($result->return == 0){
     echo "<div class='alert alert-info'>";
     echo 'Incorrect Username or Password. <meta http-equiv="refresh" content="2; url=login.php">';
@@ -16,7 +20,7 @@ else if($result->return == -1){
 }
 else if($result->return > 0){
     echo "<div class='alert alert-info'>";
-    echo 'Successfully logged in. <meta http-equiv="refresh" content="2; url=mainPage.php">';
+    echo 'Successfully logged in. <meta http-equiv="refresh" content="2; url=dashboard.php">';
     echo "</div>";
 }
 ?>
