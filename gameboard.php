72e04c15691935ea938d9301de296189574c62f7
benzobob <Auloocai1>
2019-11-18 21:52:24 +0000

    Gameboard almost finished.

--- gameboard.php
+++ gameboard.php
@@ -67,16 +67,11 @@ if (!empty($_POST["gameid"])) {
     </table>
 </div>
 
-<?php
-$result = $client->getBoard(array("gid" => $gameid));
-print_r($result->return);
-
-?>
-
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script>
+
     var num = 0;
     var x = 0;
     var y = 0;
@@ -84,29 +79,49 @@ print_r($result->return);
     var uid = <?php echo $_SESSION['id']?>;
 
 
+    //This function checks if one of the grid squares was clicked and sets the X and Y co-ordinates
     $('#grid td').click(function() {
         y = this.cellIndex;
         x = this.parentNode.rowIndex;
+        var flag = false;
 
-        $.ajax({url: "checkSquare.php?gid=" + gid + "&x=" + x + "&y=" + y, success: function(result) {
-                if(parseInt(result) === 0) {
+        /* This function checks if it is the first turn or if it is the logged in users go.
+         * Flag is set to true if it is your turn. */
+        $.ajax({url: "checkTurn.php?gid=" + gid, success: function(result) {
+            if(result==="ERROR-NOMOVES")flag = true;
+            else if (result==="ERROR-DB")flag = false;
+            else{
+                var array1 = result.split("\n");
+                var last_element = array1[array1.length - 1];
+                last_element = last_element(",");
+                flag = !(uid === parseInt(last_element[0]));
+            }
+
+            //This function checks if the square clicked is available and if it is it takes that square.
+            if(flag) {
+            $.ajax({url: "checkSquare.php?gid=" + gid + "&x=" + x + "&y=" + y, success: function (result) {
+                if (parseInt(result) === 0) {
                     $.ajax({
                         url: "takeSquare.php?pid=" + uid + "&gid=" + gid + "&x=" + x + "&y=" + y,
                         success: function (result) {
-                            var word = result;
-                            document.getElementById("" + x + "-" + y).innerHTML = word;
+                            if (parseInt(result) === 1) {
+                                var img = document.createElement("img");
+                                img.src = "imgs/x.png";
+                                var src = document.getElementById("" + x + "-" + y);
+                                src.appendChild(img);
+                                }
+                            else if (parseInt(result) === 0) window.alert("Error");
+                            else window.alert(result);
                         }
                     });
                 }
-
+                else if (parseInt(result) === 1) window.alert("This square is already taken. Please pick another.");
+                else window.alert(result);
             }});
+            }
+        }});
     });
-</script>
 
+</script>
 </body>
 </html>
-<?php
-function checkSqr($x, $y) {
-echo "Hello world!";
-}
-?>
