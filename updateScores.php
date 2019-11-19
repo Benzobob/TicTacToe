97e70cd6aef9ae9b55771e198d2bac6cef6a0643
Brian <16190319@studentmail.ul.ie>
2019-11-19 01:17:51 +0000

    refactor/ changed named from ...ajax to update. Made gameid a session variable. destroyed on dashboard page. added updateScores to update user's score page table automatically

---
+++ updateScores.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+include_once 'WebServiceClient.php';
+$result = $client->showAllMyGames(array("uid" => $_SESSION['id']));
+
+$win = 0;
+$lose = 0;
+$draw = 0;
+
+$array1 = explode("\n", $result->return);
+
+foreach ($array1 as $value) {
+    $array2 = explode("," , $value);
+    $result = $client->getGameState(array("gid" => $array2[0]));
+
+    $result = (int) $result->return;
+    if($result == 3){
+        $draw++;
+    }
+    else if ($result == 1 && $array2[1]=== $_SESSION['uname']){
+        $win++;
+    }
+    else if($result == 2 && $array2[1]=== $_SESSION['uname']){
+        $win++;
+    }
+    else{
+        $lose++;
+    }
+}
+echo <<< 'EOD'
+    <table style="width:50%">
+        <tr>
+            <th>Win</th>
+            <th>Lose</th>
+            <th>Draw</th>
+        </tr>
+        <tr>
+EOD;
+            echo "<td>" . $win  . "</td>";
+            echo "<td>" . $lose . "</td>";
+            echo "<td>" . $draw . "</td>";
+echo    "</tr>";
+echo"</table>";
+?>
